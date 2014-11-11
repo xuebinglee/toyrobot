d1675208cf72e4c80bafc9beb7014a0a7a221063
Xuebing Li <xuebinglee@gmail.com>
2014-11-11 22:45:19 +1100

    Allow bot to be placed on board

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,7 +1,13 @@
 class Robot
-  attr_reader :board
+  attr_reader :board, :x, :y,:orientation
 
   def initialize(board)
     @board = board
+  end
+
+  def place_at(x, y, orientation)
+    @x = x
+    @y = y
+    @orientation = orientation
   end
 end
--- specs/robot_spec.rb
+++ specs/robot_spec.rb
@@ -3,14 +3,37 @@ require 'board'
 require 'robot'
 
 describe Robot do
-  describe 'setup' do
-    before do
-      @board = Board.new(5, 5)
-      @robot = Robot.new(@board)
-    end
+  before do
+    @board = Board.new(5, 5)
+    @robot = Robot.new(@board)
+  end
 
+  describe 'setup' do
     it 'knows the board that it\'s on' do
       @robot.board.must_equal @board
+    end
+  end
+
+  describe '#place_at' do
+    describe 'when placed on board' do
+      before do
+        @x = rand(0..4)
+        @y = rand(0..4)
+        @orientation = [:north, :east, :south, :west].sample
+        @robot.place_at @x, @y, @orientation
+      end
+
+      it 'has the correct X' do
+        @robot.x.must_equal @x
+      end
+
+      it 'has the correct Y' do
+        @robot.y.must_equal @y
+      end
+
+      it 'has the correct orientation' do
+        @robot.orientation.must_equal @orientation
+      end
     end
   end
 end
