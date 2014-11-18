92b675020db0448e118631118be59f19caffabb6
Xuebing Li <xuebinglee@gmail.com>
2014-11-18 22:34:16 +1100

    Use constents to define default values

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
 class Board
+  DEFAULT_WIDTH  = 5
+  DEFAULT_HEIGHT = 5
+
   attr_reader :width, :height
 
   def initialize(opts = {})
-    @width  = opts[:width]  || 5
-    @height = opts[:height] || 5
+    @width  = opts[:width]  || DEFAULT_WIDTH
+    @height = opts[:height] || DEFAULT_HEIGHT
   end
 end
