e718ef2d79fcb99291d089389f6727d1818abf61
Xuebing Li <xuebinglee@gmail.com>
2014-11-18 23:26:19 +1100

    Refactor run script
    Run method should only be executed when the script is the main file being run instead of being required or loaded by another file

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/env ruby
 require_relative '../lib/parser'
 
-parser = Parser.new
-ARGF.each do |line|
-  command = line.rstrip
-  parser.parse command
+def run
+  parser = Parser.new
+  ARGF.each do |line|
+    command = line.rstrip
+    parser.parse command
+  end
+end
+
+if __FILE__ == $0
+  run
 end
