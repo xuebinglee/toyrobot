eb6b44cc4ecee8e40e4e46c46f24728711e38538
Xuebing Li <xuebinglee@gmail.com>
2016-08-06 22:57:00 +1000

    refactor(Robot): call method instead of instance variable to mutate

--- lib/robot.rb
+++ lib/robot.rb
@@ -10,50 +10,54 @@ class Robot
     new board: Board.new
   end
 
-  def initialize(opts)
-    @board = opts[:board]
+  def initialize(board:)
+    @board = board
   end
 
-  def place_at(opts)
-    @geometry = Geometry.new board: @board,
-                             x: opts[:x],
-                             y: opts[:y],
-                             orientation: opts[:orientation]
+  def place_at(x:, y:, orientation:)
+    @geometry = Geometry.new(board: board,
+                             x: x,
+                             y: y,
+                             orientation: orientation)
   rescue ArgumentError # Geometry is illegal
     @geometry = nil
   end
 
   def turn_left
-    return unless @geometry # Ignore command unless robot is properly placed
+    return unless geometry # Ignore command unless robot is properly placed
     # index of previous direction in clockwise ORIENTATIONS
-    i = ORIENTATIONS.index(@geometry.orientation) - 1
+    i = ORIENTATIONS.index(geometry.orientation) - 1
     @geometry.orientation = ORIENTATIONS[i]
   end
 
   def turn_right
-    return unless @geometry # Ignore command unless robot is properly placed
+    return unless geometry # Ignore command unless robot is properly placed
     # index of next direction in clockwise ORIENTATIONS
     # Left shift index by 4 so that it doesn't reach 4 which is illegal
-    i = ORIENTATIONS.index(@geometry.orientation) + 1 - 4
+    i = ORIENTATIONS.index(geometry.orientation) + 1 - 4
     @geometry.orientation = ORIENTATIONS[i]
   end
 
   def move
-    return unless @geometry # Ignore command unless robot is properly placed
-    case @geometry.orientation
+    return unless geometry # Ignore command unless robot is properly placed
+    case geometry.orientation
     when :north
-      @geometry.y += 1
+      @geometry.y = geometry.y + 1
     when :south
-      @geometry.y -= 1
+      @geometry.y = geometry.y - 1
     when :east
-      @geometry.x += 1
+      @geometry.x = geometry.x + 1
     when :west
-      @geometry.x -= 1
+      @geometry.x = geometry.x - 1
     end
   end
 
   def report
-    return unless @geometry # Ignore command unless robot is properly placed
-    puts "#{@geometry.x},#{@geometry.y},#{@geometry.orientation.upcase}"
+    return unless geometry # Ignore command unless robot is properly placed
+    puts "#{geometry.x},#{geometry.y},#{geometry.orientation.upcase}"
   end
+
+  private
+
+  attr_reader :board
 end
